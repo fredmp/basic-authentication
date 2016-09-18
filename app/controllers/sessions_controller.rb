c69fcd4795c7a2c7ba8394de275f35cb74de4c2b
Frederico <martinsporto@gmail.com>
2016-09-17 21:48:09 -0300

    Create session controller and basic views

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    api_response = UserApiMock.new.authenticate(allowed_params[:email], allowed_params[:password])
+
+    if api_response[:success]
+      session[:user_id] = api_response[:user][:id]
+      redirect_to root_path, notice: api_response[:message]
+    else
+      redirect_to root_path, alert: api_response[:message]
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/login', notice: I18n.t(:logout_message)
+  end
+
+  private
+
+  def allowed_params
+    params.require(:session).permit([:email, :password])
+  end
+
+end
